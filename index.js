7143c54885c36d814143d24f9ffd4da9197ba95f
harenarium <carolinel2610@gmail.com>
2017-11-02 02:42:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } else if(string===string.toUpperCase){
     return "YES INDEED!"
-  } else if (string===string.toUpperCase){
+  } else if (string==="I love you, Grandma."){
     return "YES INDEED!"
     
   }
